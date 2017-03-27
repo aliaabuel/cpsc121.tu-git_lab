510697f69fd5edd0ef769c9911daa03aa2b42f13
JoseSanrindo <takashisk@csu.fullerton.edu>
2017-03-26 23:34:15 -0700

    added jsanrindo.h

---
+++ headers/jsanrindo.h
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void colors()
+{
+ int color;
+ srand(time(0))
+ a = rand%13
+ switch (color)
+ {
+  case 0:
+  cout << "red" << endl;
+break;
+  case 1:
+  cout << "red" << endl;
+break;
+  case 2:
+  cout << "blue" << endl;
+break;
+  case 3:
+  cout << "green" << endl;
+break;
+  case 4:
+  cout << "purplee" << endl;
+break;
+  case 5:
+  cout << "yellow" << endl;
+break;
+  case 6:
+  cout << "orange" << endl;
+break;
+  case 7:
+  cout << "pink" << endl;
+break;
+  case 8:
+  cout << "cyan" << endl;
+break;
+  case 9:
+  cout << "magenta" << endl;
+break;
+  case 10:
+  cout << "teal" << endl;
+break;
+  case 11:
+  cout << "black" << endl;
+break;
+  case 12:
+  cout << "white" << endl;
+break;
+}
+}
