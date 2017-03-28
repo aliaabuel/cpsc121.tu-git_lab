bc0344a74bb6a4743bd85fb2c2975c62d745919c
smlnchcn <simi.chacon7@gmail.com>
2017-03-28 12:21:24 -0700

    counting characters from a file

---
+++ headers/schacon.h
@@ -0,0 +1,30 @@
+// Simi Chacon
+// Count the number of characters in a file
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void count_char();
+
+void count_char()
+{
+    string my_char;
+    ifstream inFile;
+    int i;
+    
+    inFile.open("my_text.txt")
+    
+    if(inFile.is_open())
+    {
+        while (getline(inFile, my_char)
+               {
+                   cout<< my_char.length()<< endl;
+                   i += my_char.length();
+               }
+    }
+    
+               inFile.close();
+    
+}
